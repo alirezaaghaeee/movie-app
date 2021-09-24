b14bd9beb62019fa3ff0a720bd4281d65d50e2dd
alireza <alirezaaghaee77jj@gmail.com>
2021-09-24 10:27:03 +0330

    Deleting extra files and connecting to api

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,31 @@
 <template>
-    <div class="home">hello</div>
+  <div class="home">
+    <Hero />
+  </div>
 </template>
+
+<script>
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      movies: [],
+    }
+  },
+  async fetch(){
+    await this.getmovie()
+  },
+  methods: {
+    async getmovie() {
+      const data = axios.get(
+        'https://api.themoviedb.org/3/movie/now_playing?api_key=def5785d32d90d07ffbece2bba421a78&language=en-US&page=1'
+      )
+      const result=await data;
+      console.log(result.data);
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
